4aab4896450991b64f008ddd55e0db366141ca3d
Saber <92962589+smit4k@users.noreply.github.com>
2025-01-02 23:53:08 -0500

    Add user prompts for boilerplate language selection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::fs;
+use std::io::{self,Write};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Boilrs --- A boilerplate generator written in Rust");
+    println!("Supported languages: Rust, Python, Javascript");
+    println!("Enter the language you would want to generate boilerplate code for:")
+
+
 }
